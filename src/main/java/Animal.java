e65fbb6aec21d41cdc22b8a5ba9661db27b160ab
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-05 23:16:13 +0300

    I have added the method to clear the arraylist

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,15 +1,19 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Animal {
 private String name;
 private String ranger;
 private String status;
 private LocalDateTime sighted;
+private static List<Animal> instances=new ArrayList<Animal>();
 public Animal(String name,String ranger,String status){
         this.name=name;
         this.ranger=ranger;
         this.status=status;
         sighted=LocalDateTime.now();
+        instances.add(this);
 }
 
 public String getName(){
@@ -23,5 +27,11 @@ public String getStatus(){
 }
 public LocalDateTime getCreatedAt(){
         return sighted;
+}
+public static List<Animal> all(){
+  return instances;
+}
+public static void clear(){
+  instances.clear();
 }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -22,4 +22,17 @@ public void getCreatedAt_getsWhenAnimalWasInstantiated_now(){
   Animal myAnimal=new Animal("lion","collins","endangered");
   assertEquals(LocalDateTime.now().getDayOfWeek(),myAnimal.getCreatedAt().getDayOfWeek());
 }
+@Test
+public void all_returnsAllInstancesOfAnimal_true(){
+  Animal myAnimal=new Animal("lion","collins","endangered");
+  Animal secondAnimal=new Animal("tiger","collins","endangered");
+  assertEquals(true,Animal.all().contains(myAnimal));
+  assertEquals(true,Animal.all().contains(secondAnimal));
+}
+@Test
+public void clear_clearsTheArrayWithAnimals_true(){
+  Animal myAnimal=new Animal("lion","collins","endangered");
+  Animal.clear();
+  assertEquals(0,Animal.all().size());
+}
 }
