544720038cd88acf43be109d4fba78c17773acb0
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-05 21:46:16 +0300

    I have added getter methods for the sighting class and they work

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,5 +1,22 @@
-public class Sighting{
-  public Sighting(){
-    
-  }
+public class Sighting {
+private String name;
+private String location;
+private String time;
+
+public Sighting(String name,String location,String time){
+        this.name=name;
+        this.location=location;
+        this.time=time;
+}
+
+public String getName(){
+        return name;
+
+}
+public String getLocation(){
+        return location;
+}
+public String getTime(){
+        return time;
+}
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -4,7 +4,12 @@ import static org.junit.Assert.*;
 public class SightingTest{
   @Test
   public void InstantiatesCorrectly_true(){
-    Sighting newSighting=new Sighting();
+    Sighting newSighting=new Sighting("firstSighting","riverbed","morning");
     assertTrue(newSighting instanceof Sighting);
+  }
+  @Test
+  public void getterMethodsWorkWell(){
+    Sighting newSighting=new Sighting("firstSighting","riverbed","morning");
+    assertEquals("firstSighting",newSighting.getName());
   }
 }
