896b89e72b273cc60fd5f57a1f1c843d156b5c7f
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-07 23:07:58 +0300

    I have added the template to display the sightings

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -33,6 +33,14 @@ public static void main(String[] args) {
                      model.put("template","/templates/success.vtl");
                      return new ModelAndView(model,layout);
              },new VelocityTemplateEngine());
+
+        get("/sightings", (request,response) ->{
+                    Map<String, Object> model = new HashMap<String, Object>();
+                    model.put("sightings", Sighting.all());
+                    model.put("template","templates/sightings.vtl");
+                    return new ModelAndView(model,layout);
+
+            },new VelocityTemplateEngine());
 }
 
 
